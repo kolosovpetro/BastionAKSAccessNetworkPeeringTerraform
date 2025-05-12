a8ab4a006044a22fbcb0e7a6a189953abc7a413b
kolosovpetro <kolosovp94@gmail.com>
2025-05-12 21:02:33 +0200

    configure private link

---
+++ Configure-Private-DNS-Zone.ps1
@@ -0,0 +1,16 @@
+﻿$AksNodeRg = $( terraform output -raw aks_node_rg )
+$SubscriptionId = $( terraform output -raw subscription_id )
+$SpokeRg = $( terraform output -raw spoke_rg )
+$SpokeVnet = $( terraform output -raw spoke_vnet )
+
+$DnsZoneName = az network private-dns zone list --resource-group "$aksNodeRG" `
+    --query "[?contains(name, 'privatelink')].name | [0]" --output tsv
+
+Write-Host "Dns Zone name: $DnsZoneName"
+
+az network private-dns link vnet create `
+--resource-group "$aksNodeRG" `
+--zone-name "$DnsZoneName" `
+--name "aks-dns-link" `
+--virtual-network "/subscriptions/$SubscriptionId/resourceGroups/$SpokeRg/providers/Microsoft.Network/virtualNetworks/$SpokeVnet" `
+--registration-enabled false
--- output.tf
+++ output.tf
@@ -1,3 +1,19 @@
 output "connect_command" {
   value = "az aks get-credentials --resource-group ${azurerm_resource_group.public.name} --name ${azurerm_kubernetes_cluster.aks.name} --subscription ${data.azurerm_client_config.current.subscription_id}"
 }
+
+output "aks_node_rg" {
+  value = azurerm_kubernetes_cluster.aks.node_resource_group
+}
+
+output "subscription_id" {
+  value = data.azurerm_subscription.current.subscription_id
+}
+
+output "spoke_rg" {
+  value = azurerm_resource_group.spoke.name
+}
+
+output "spoke_vnet" {
+  value = azurerm_virtual_network.spoke.name
+}
